da66f758f6f6c5e27099b886251501fe4364b24b
Loveless <abonde16@student.aau.dk>
2019-10-01 09:18:17 +0200

    first commit

---
+++ find-fejl.sh
@@ -0,0 +1,5 @@
+grep " man " -rIi --include=*.tex
+grep " jeg " -rIi --include=*.tex
+grep " vi " -rIi --include=*.tex
+grep " fx " -rIi --include=*.tex
+grep "\\\\cite{[a-zA-Z0-9]*}\\s*\\\\cite{[a-zA-Z0-9]*}" -rI --include=*.tex
