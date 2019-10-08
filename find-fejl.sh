90a07163f38b381afa24a4d960b76d3459ad054f
Rasmus Rendal <rasmus@rend.al>
2019-10-08 10:25:40 +0200

    Fællesretning

--- find-fejl.sh
+++ find-fejl.sh
@@ -2,5 +2,6 @@ grep " man " -rIi --include=*.tex
 grep " jeg " -rIi --include=*.tex
 grep " vi " -rIi --include=*.tex
 grep " fx " -rIi --include=*.tex
+grep " we " -rIi --include=*.tex
 grep " Pia " -rIi --include=*.tex
 grep "\\\\cite{[a-zA-Z0-9]*}\\s*\\\\cite{[a-zA-Z0-9]*}" -rI --include=*.tex
