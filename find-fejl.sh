d976f310ca1592e74861fd2575926a7789324021
Henrik Christensen <sensen1695@hotmail.com>
2019-10-29 10:48:43 +0100

    added new rules to find-fejl

--- find-fejl.sh
+++ find-fejl.sh
@@ -1,7 +1,5 @@
-grep " man " -rIi --include=*.tex
-grep " jeg " -rIi --include=*.tex
-grep " vi " -rIi --include=*.tex
-grep " fx " -rIi --include=*.tex
 grep " we " -rIi --include=*.tex
 grep " Pia " -rIi --include=*.tex
+grep "\\\\newline" -rIi --include=*.tex
+grep '\\\\' -rIi --include=*.tex
 grep "\\\\cite{[a-zA-Z0-9]*}\\s*\\\\cite{[a-zA-Z0-9]*}" -rI --include=*.tex
